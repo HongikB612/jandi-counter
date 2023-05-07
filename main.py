8aeb939d48f0701087795af57d490bfe67cb042b
nx006 <limazero14@gmail.com>
2023-05-08 02:36:22 +0900

    Feat Add crawler.py
    Implement crawler and store files functions

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+from src import crawler
+
+load_dotenv()
+
+if __name__ == '__main__':
+    organization_name: str = 'HongikB612'  # Replace with the actual organization name
+    token: str = os.getenv('GITHUB_TOKEN')
+    start_date_str: str = '2023-05-03'  # Replace with the actual start date of your event
+    num_weeks: int = 2
+
+    usernames: list[str] = crawler.fetch_members_from_organization(organization_name, token)
+    start_date: datetime = datetime.strptime(start_date_str, '%Y-%m-%d')
+    contribution_data = {}
+
+    for username in usernames:
+        contributions = crawler.get_contribution_data(username, start_date)
+        weekly_contributions = crawler.calculate_weekly_contributions(contributions, start_date, num_weeks)
+        contribution_data[username] = contributions
+
+    crawler.save_daily_contributions_to_file(usernames, contribution_data)
+
+    for username, weekly_contributions in contribution_data.items():
+        print(f'{username}: {weekly_contributions}')
---
+++ src/crawler.py
@@ -0,0 +1,95 @@
+import csv
+import re
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_contribution_data(username: str, event_start_date: datetime):
+    url = f'https://github.com/{username}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    days = soup.find_all('rect', class_='ContributionCalendar-day')
+
+    contributions = []
+    for day in days:
+        if 'data-date' not in day.attrs:
+            break
+
+        date = datetime.strptime(day['data-date'], '%Y-%m-%d').date()
+        if date >= event_start_date.date():
+            count_text = day.get_text()
+            count = extract_contribution_count(count_text)
+            contributions.append({'date': date, 'count': count})
+
+    return contributions
+
+
+def extract_contribution_count(count_text: str):
+    match = re.search(r'(\d+) contributions', count_text)
+    if match:
+        return int(match.group(1))
+    else:
+        return 0
+
+
+def calculate_weekly_contributions(contributions, start_date: datetime, num_weeks: int) -> list[int]:
+    weekly_contributions = [0] * num_weeks
+
+    for contribution in contributions:
+        date = contribution['date']
+        count = contribution['count']
+        if start_date.date() <= date:
+            week_index = (date - start_date.date()).days // 7
+            if week_index < num_weeks:
+                weekly_contributions[week_index] += count
+
+    return weekly_contributions
+
+
+def save_daily_contributions_to_file(usernames: list[str], contribution_data: dict):
+    with open('daily_contributions.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Date'] + usernames
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        all_dates = set()
+        for daily_contributions in contribution_data.values():
+            for entry in daily_contributions:
+                all_dates.add(entry['date'])
+
+        for date in sorted(all_dates):
+            row = {'Date': date}
+            for username in usernames:
+                row[username] = 0
+                for daily_contribution in contribution_data[username]:
+                    if daily_contribution['date'] == date:
+                        row[username] = daily_contribution['count']
+                        break
+            writer.writerow(row)
+
+
+
+def fetch_members_from_organization(organization_name: str, token: str):
+    api_url = f'https://api.github.com/orgs/{organization_name}/members'
+    headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github+json'}
+    usernames = []
+    page = 1
+
+    while True:
+        response = requests.get(api_url, headers=headers, params={'page': page})
+
+        if response.status_code != 200:
+            print(f'Error fetching members from organization {organization_name}: {response.status_code}')
+            print(response.content)
+            break
+
+        members = response.json()
+        if not members:
+            break
+
+        usernames.extend([member['login'] for member in members])
+        page += 1
+
+    return usernames
